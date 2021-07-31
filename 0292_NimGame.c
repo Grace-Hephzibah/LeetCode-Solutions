051bd6374f79e40c6cb910bcf1b027d7a4609c60
Grace Hephzibah M <63595093+Grace-Hephzibah@users.noreply.github.com>
2021-07-31 11:49:27 +0530

    Update 0292_NimGame.c

--- 0292_NimGame.c
+++ 0292_NimGame.c
@@ -1,6 +1,16 @@
-
+// Solution 1
+/*
 bool canWinNim(int n)
 {    
     if(n%4 == 0) return false;
     else return true;
+}
+*/
+
+// Solution 2 - Even Better !!!
+
+#define print (n%4)?true:false
+bool canWinNim(int n)
+{    
+    return (print);
 }
